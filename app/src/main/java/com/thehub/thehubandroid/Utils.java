aaf0857619562a18f6ca2cd2a3eb15ae44e11790
Robbie <robsmall@umich.edu>
2014-10-02 21:55:16 -0400

    fixed utils string. login now works.

--- app/src/main/java/com/thehub/thehubandroid/Utils.java
+++ app/src/main/java/com/thehub/thehubandroid/Utils.java
@@ -10,5 +10,5 @@ public class Utils {
     // To get your test ip run "ifconfig" in your terminal and get the
     // address that looks like the one below. It will be for eth1
     public static String IP_TEST = "http://10.0.0.44:5000";
-    public static String IP_PROD = "http://the-hubba-backend.herokuapp.com";
+    public static String IP_PROD = "http://the-hub-backend.herokuapp.com";
 }
