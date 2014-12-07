71b1f43bb14e04a61c53e4e5a3a2464103b47456
Robbie <robsmall@umich.edu>
2014-12-06 19:38:09 -0500

    gcm works

---
+++ app/src/main/java/com/thehub/thehubandroid/GcmBroadcastReceiver.java
@@ -0,0 +1,25 @@
+package com.thehub.thehubandroid;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+
+/**
+ * Receives a message from GCM
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("DEBUG", "PUSH RECEIVED!");
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
